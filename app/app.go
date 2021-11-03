8690e7e8b49bdccc9dad56a12bae9af2c5345a84
Matheus Antunes <matheus.antunes@loanboox.ch>
2021-11-03 09:11:53 +0100

    flag and add starting log

--- app/app.go
+++ app/app.go
@@ -10,20 +10,20 @@ import (
 )
 
 func main() {
-	port := *flag.Int("port", 7, "echo tcp port")
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	port := flag.Int("port", 7, "echo tcp port")
+	flag.Parse()
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
-	for err != nil {
+	log.Printf("Listening on port %v", *port)
+	for {
 		con, err := ln.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		go echo.Do(con)
-	}
-	err = ln.Close()
-	if err != nil {
-		log.Fatal(err)
+		if err := echo.Do(con); err != nil {
+			log.Println(err)
+		}
 	}
 }
