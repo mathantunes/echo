088e716f97288bd9ab541f79e0311f253401f358
Matheus Antunes <matheus.antunes@loanboox.ch>
2021-11-03 09:12:31 +0100

    close writer

--- test/test.go
+++ test/test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -9,22 +11,30 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":7")
-	if err != nil {
-		log.Fatal(err)
-	}
+	port := flag.Int("port", 7, "echo tcp port")
+	flag.Parse()
 	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-	_, err = conn.Write([]byte(text))
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = io.Copy(os.Stdout, conn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = conn.Close()
-	if err != nil {
-		log.Fatal(err)
+	for {
+		conn, err := net.Dial("tcp", fmt.Sprintf(":%v", *port))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		text, _ := reader.ReadString('\n')
+		_, err = conn.Write([]byte(text))
+		if tcpcon, ok := conn.(*net.TCPConn); ok {
+			tcpcon.CloseWrite()
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.Copy(os.Stdout, conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
